72d87149a4c458d1780e598206b9fff8dc16dca8
tz <tz@tribepayments.com>
2022-10-19 14:56:38 +0300

    add all endpoints

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"log"
 	"net/http"
@@ -14,6 +13,45 @@ func main() {
 	r.Use(middleware.Logger)
 
 	r.Get("/hello", func(w http.ResponseWriter, r *http.Request) {
+		//write response
+	})
+
+	r.Get("/ingredients", func(w http.ResponseWriter, r *http.Request) {
+		//json.NewEncoder(w).Encode(...)
+		//write response
+	})
+
+	r.Post("/order-buns", func(w http.ResponseWriter, r *http.Request) {
+		//json.NewDecoder(...).Decode(...)
+		//write response
+	})
+
+	r.Post("/slice", func(w http.ResponseWriter, r *http.Request) {
+		//for i := 1; i < 10; i++ {
+		//
+		//}
+		//write response
+	})
+
+	r.Post("/slice-faster", func(w http.ResponseWriter, r *http.Request) {
+		//var wg sync.WaitGroup
+		//wg.Add(1)
+		//wg.Done()
+		//wg.Wait()
+		//write response
+	})
+
+	r.Post("/mix", func(w http.ResponseWriter, r *http.Request) {
+		//write response
+	})
+
+	r.Post("/cook", func(w http.ResponseWriter, r *http.Request) {
+		//write response
+	})
+
+	r.Post("/assemble", func(w http.ResponseWriter, r *http.Request) {
+		//b := burger.New(burger.Ingredients())
+		//w.Write([]byte(b.Assemble()))
 		//write response
 	})
 
